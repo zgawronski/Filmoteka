6c3ccae28487e93b0cc42918f41791a4d5d3ef91
Zbigniew Gawroński <zbigniew.gawronski@microsoft.wsei.edu.pl>
2021-02-02 17:04:47 +0100

    dodanie bazy

--- Filmoteka/MainWindow.xaml.cs
+++ Filmoteka/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Filmoteka
 {
@@ -20,9 +11,46 @@ namespace Filmoteka
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly FilmContext _context =
+            new FilmContext();
+
+        private CollectionViewSource categoryViewSource;
         public MainWindow()
         {
             InitializeComponent();
+            categoryViewSource =
+                (CollectionViewSource)FindResource(nameof(categoryViewSource));
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // this is for demo purposes only, to make it easier
+            // to get up and running
+            _context.Database.EnsureCreated();
+
+            // load the entities into EF Core
+            _context.Categories.Load();
+
+            // bind to the source
+            categoryViewSource.Source =
+                _context.Categories.Local.ToObservableCollection();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // all changes are automatically tracked, including
+            // deletes!
+            _context.SaveChanges();
+
+            // this forces the grid to refresh to latest values
+            categoryDataGrid.Items.Refresh();
+            filmsDataGrid.Items.Refresh();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            // clean up database connections
+            _context.Dispose();
+            base.OnClosing(e);
         }
     }
 }
